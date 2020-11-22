a02c818b163c98b12fbf0443e25c67fbca536b20
Yusuf <yusufgurdogan@protonmail.com>
2020-11-22 20:03:03 +0000

    Updates

--- assets/lib/cpupow.js
+++ assets/lib/cpupow.js
@@ -1,7 +1,7 @@
-const BademCurrency = require('nanocurrency')
+const NanoCurrency = require('nanocurrency')
 // When the parent theard requires it, render the HTML
 self.addEventListener("message", async (message) => {
   const { blockHash, workerIndex, workerCount, workThreshold } = message.data;
-  const result = await BademCurrency.computeWork(blockHash, { workThreshold, workerIndex, workerCount });
+  const result = await NanoCurrency.computeWork(blockHash, { workThreshold, workerIndex, workerCount });
   self.postMessage(result);
 });
